6d3df0686781866b4c67a8cc6ccf65cccbc10511
SOSREBET <kekcij@mail.ru>
2023-09-17 20:37:01 +0300

    Обувь

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,52 @@
-from django.contrib import admin
+# from django import forms
+# from django.contrib import admin
+# from ckeditor_uploader.widgets import CKEditorUploadingWidget
+# from ckeditor.widgets import CKEditorWidget
 
-# Register your models here.
+# from .models import CourseCategoriesModel, CourseModel, CourseCommentModel
+
+# # Register your models here.
+
+
+# class CourseCategoriesAdmin(admin.ModelAdmin):
+#     list_display = ("image_icon_tag", "name", "slug")
+#     fields = ("image_tag", "name", "photo", "slug")
+#     readonly_fields = ("image_tag", )
+#     search_fields = ("name", )
+#     prepopulated_fields = {"slug": ("name", )}
+
+
+# class CourseAdminForm(forms.ModelForm):
+#     course_content = forms.CharField(widget=CKEditorUploadingWidget(config_name='uploader'))
+#     course_more = forms.CharField(widget=CKEditorWidget(), required=False)
+
+#     class Meta:
+#         model = CourseModel
+#         fields = "__all__"
+
+
+# class CourseAdmin(admin.ModelAdmin):
+#     list_display = ("category", "course_name", "author", "likes", "is_private","datetime_upload")
+#     readonly_fields = ("image_tag", "datetime_upload")
+#     search_fields = ("category", "course_name", "author", "course_author", "course_download_link", "datetime_upload")
+#     prepopulated_fields = {"slug": ("course_author", "course_name")}
+#     form = CourseAdminForm
+
+
+# class CourseCommentsAdminForm(forms.ModelForm):
+#     text = forms.CharField(widget=CKEditorUploadingWidget(config_name='uploader'))
+#     class Meta:
+#         model = CourseCommentModel
+#         fields = "__all__"
+
+
+# class CourseCommentsAdmin(admin.ModelAdmin):
+#     list_display = ("text", "course")
+#     readonly_fields = ("date_upload", )
+#     search_fields = ("text", "course")
+#     form = CourseCommentsAdminForm
+
+
+# admin.site.register(CourseCategoriesModel, CourseCategoriesAdmin)
+# admin.site.register(CourseModel, CourseAdmin)
+# admin.site.register(CourseCommentModel, CourseCommentsAdmin)
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+
+GENDER = [
+    ('M', 'Мужской'),
+    ('F', 'Женский')
+]
+
+SPORT_TYPE = [
+    ('H', 'Поход'),
+    ('R', 'Бег'),
+    ('F', 'Фитнес'),
+    ('S', 'Лыжи'),
+]
+
+SEASON = [
+    ('W', 'Зима'),
+    ('S', 'Весна'),
+    ('S', 'Лето'),
+    ('A', 'Осень'),
+]
+
+class BrandModel(models.Model):
+    title = models.CharField('Название', max_length=128, unique=True)
+    manufacturer_code = models.CharField('Код производителя', max_length=7, unique=True)
+    country = models.CharField('Страна производителя', max_length=64, unique=True)
+
+
+class ShoesModel(models.Model):
+    title = models.CharField('Название', max_length=128, unique=True)
+    price = models.PositiveIntegerField('Цена')
+    description = models.TextField('Описание', max_length=2048)
+    
+    insulation_weight = models.PositiveIntegerField('Вес утеплителя, г/м2')
+    clasp = models.CharField('Застежка', max_length=64)
+    reinforced_bumper = models.BooleanField('Усиленный бампер')    
+    
+    antibacterial_impregnation = models.CharField('Антибактериальная пропитка', max_length=64)
+    moisture_protection = models.CharField('Защита от влаги', max_length=64)
+    
+    gender = models.CharField('Пол', max_length=1, choices=GENDER)
+    sport_type = models.CharField('Вид спорта', max_length=1, choices=SPORT_TYPE)
+    season = models.CharField('Сезон', max_length=1, choices=SEASON)
+    
+    guarantee_period = models.PositiveIntegerField('Срок гарантии')
+    product_authenticity_guarantee = models.BooleanField('Гарантия подлинности товара', max_length=64)
+    category =  models.CharField('Категория', max_length=64)
+
+    upper_material = models.CharField('Материал верха', max_length=64)
+    lining_material = models.CharField('Материал подкладки', max_length=64)
+    outsole_material = models.CharField('Материал подошвы', max_length=64)
+    insole_material = models.CharField('Материал стельки', max_length=64)
+    
+    brand = models.ForeignKey(BrandModel, on_delete=models.CASCADE, verbose_name="Бренд", related_name='brand')
+
+
+class ShoesPhotoModel(models.Model):
+    photo = models.ImageField('Фото', upload_to='shoes/')
+    shoes = models.ForeignKey(ShoesModel, on_delete=models.CASCADE, verbose_name="К обуви", related_name='photos')
+
+
+# Остальные
+
+
+
+# class Technology(models.Model):
+#     pass
+    # many-to-many 
